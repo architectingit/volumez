bf2ed91ffefb270ce29a1c51b9aca9bb50f75bfe
architectingit <61459687+architectingit@users.noreply.github.com>
2023-05-31 09:50:06 +0100

    Update run-db-test.sh

--- run-db-test.sh
+++ run-db-test.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env sh
 set -e
 
-if [ -z $DBENCH_MOUNTPOINT ]; then
-    DBENCH_MOUNTPOINT=/mnt/vol1
-fi
-
-    echo "tersever,fiover,testname,blocksize,iodepth,readkb,readbw,readiops,readbwmin,readbwmax,readbw%,readbwmean,readbwstd,writekb,writebw,writeiops,writebwmin,writebwmax,writebw%,writebwmean,writebwstd,iod1,iod2,iod4,iod8,iod16,iod32,iod64"
+    echo "tersever,fiover,testname,blocksize,iodepth,readkb,readbw,readiops,readlatmin,readlatmax,readlatmean,readlatstdev,readbwmin,readbwmax,readbw%,readbwmean,readbwstd,writekb,writebw,writeiops,writelatmin,writelatmax,writelatmean,writelatstdev,writebwmin,writebwmax,writebw%,writebwmean,writebwstd,iod1,iod2,iod4,iod8,iod16,iod32,iod64"
     echo 1 > /proc/sys/vm/drop_caches
-    FILEDIR=$DBENCH_MOUNTPOINT fio --output-format=terse db-tests.fio > /tmp/db-test.tmp
-    cut -d";" --fields 1-3,6-8,42-49,83-87,93-99 /tmp/db-test.tmp
-    FILEDIR=$DBENCH_MOUNTPOINT fio --output-format=terse ai-tests.fio > /tmp/ai-test.tmp
-    cut -d";" --fields 1-3,6-8,42-49,83-87,93-99 /tmp/ai-test.tmp
-    FILEDIR=$DBENCH_MOUNTPOINT fio --output-format=terse hpc-tests.fio > /tmp/hpc-test.tmp
-    cut -d";" --fields 1-3,6-8,42-49,83-87,93-99 /tmp/hpc-test.tmp
+    fio --output-format=terse db-tests.fio > /tmp/db-test.tmp
+    cut -d";" --fields 1-3,6-8,38-49,79-87,93-99 /tmp/db-test.tmp
+    fio --output-format=terse ai-tests.fio > /tmp/ai-test.tmp
+    cut -d";" --fields 1-3,6-8,38-49,79-87,93-99 /tmp/ai-test.tmp
+    fio --output-format=terse hpc-tests.fio > /tmp/hpc-test.tmp
+    cut -d";" --fields 1-3,6-8,38-49,79-87,93-99 /tmp/hpc-test.tmp
     rm /tmp/db-test.tmp
     rm /tmp/ai-test.tmp
     rm /tmp/hpc-test.tmp
-    rm $DBENCH_MOUNTPOINT/fiotest
     exit 0
