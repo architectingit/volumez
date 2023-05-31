6288e83a866a253adce43fda933f5d5a029ae7fb
architectingit <61459687+architectingit@users.noreply.github.com>
2023-05-31 10:18:42 +0100

    Create pull-scripts.sh

---
+++ pull-scripts.sh
@@ -0,0 +1,8 @@
+wget https://raw.githubusercontent.com/architectingit/volumez/main/run-db-test.sh
+wget https://raw.githubusercontent.com/architectingit/volumez/main/ai-tests.fio
+wget https://raw.githubusercontent.com/architectingit/volumez/main/ai.fio
+wget https://raw.githubusercontent.com/architectingit/volumez/main/db-tests.fio
+wget https://raw.githubusercontent.com/architectingit/volumez/main/db.fio
+wget https://raw.githubusercontent.com/architectingit/volumez/main/global.fio
+wget https://raw.githubusercontent.com/architectingit/volumez/main/hpc-tests.fio
+wget https://raw.githubusercontent.com/architectingit/volumez/main/hpc.fio
